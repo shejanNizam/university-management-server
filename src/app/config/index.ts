b30def42cd5fc8b7b7d2677a61dfc02a294babd7
nizam <nizam.shejan11@gmail.com>
2025-12-21 15:04:55 +0600

    initially setup basic server and configure

---
+++ src/app/config/index.ts
@@ -0,0 +1,4 @@
+export const configs = {
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+};
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import app from "./app";
+import { configs } from "./app/config";
+
+async function main() {
+  try {
+    await mongoose.connect(configs.database_url as string);
+
+    app.listen(7000, () => {
+      console.log(
+        `University Management Server is running on port ${configs.port}`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
