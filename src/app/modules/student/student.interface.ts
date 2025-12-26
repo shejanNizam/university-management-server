b6cc18e4f56a9e5981bac2b4c8afb20a4fec7116
nizam <nizam.shejan11@gmail.com>
2025-12-26 12:27:27 +0600

    create student initial interface

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,40 @@
+export type TStudentName = {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+};
+
+export type TGuardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type TLocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type TStudent = {
+  id: string;
+  password: string;
+  name: TStudentName;
+  gender: "male" | "female" | "other";
+  dateOfBirth?: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: TGuardian;
+  localGuardian: TLocalGuardian;
+  profileImage: string;
+  isActive: "active" | "blocked";
+  isDeleted: boolean;
+};
