3b87e92b55b8361a741adeae2ddc195e77edd22f
shejanNizam <nizam.shejan11@gmail.com>
2026-01-07 09:53:16 +0600

    customize al moduler routes in separate file

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,7 @@ import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
-import { StudentRoutes } from "./app/modules/student/student.route";
-import { UserRoutes } from "./app/modules/user/user.route";
+import router from "./app/routes";
 
 const app: Application = express();
 
@@ -11,12 +10,14 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1/students", StudentRoutes);
-app.use("/api/v1/users", UserRoutes);
+// application routes
+app.use("/api/v1", router);
 
-app.get(`/`, (req: Request, res: Response) => {
+const test = (req: Request, res: Response) => {
   res.send(`Hello from University Management Server`);
-});
+};
+
+app.get(`/`, test);
 
 // global error handler
 app.use(globalErrorHandler);
